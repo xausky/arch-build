ede8fcfacb0d59ad3424c007dfd77b59bda1a8a0
xausky <xausky@163.com>
2022-06-14 16:08:07 +0800

    update

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -7,6 +7,6 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
 pacman -Sy --noconfirm
-pacman -S --noconfirm yay
+pacman -U --noconfirm /yay.pkg.tar.zst
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
