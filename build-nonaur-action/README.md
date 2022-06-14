7787dba4bc8c55c088d9b8773d557b46a63296f0
xausky <xausky@gmail.com>
2022-06-14 15:34:29 +0800

    update

--- build-nonaur-action/README.md
+++
@@ -1,1 +0,0 @@
-This is a fork version of [edlanglois/pkgbuild-action](https://github.com/edlanglois/pkgbuild-action).
--- create-db-and-upload-action/entrypoint.sh
+++ create-db-and-upload-action/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -e
 
 init_path=$PWD
 mkdir upload_packages
@@ -18,23 +18,7 @@ if [ ! -f ~/.config/rclone/rclone.conf ]; then
     echo "drive_id=$RCLONE_ONEDRIVE_DRIVE_ID" >> ~/.config/rclone/rclone.conf
 fi
 
-if [ ! -z "$gpg_key" ]; then
-    echo "$gpg_key" | gpg --import
-fi
-
 cd upload_packages || exit 1
 
 repo-add "./${repo_name:?}.db.tar.gz" ./*.tar.zst
-python3 $init_path/create-db-and-upload-action/sync.py
-rm "./${repo_name:?}.db.tar.gz"
-rm "./${repo_name:?}.files.tar.gz"
-
-if [ ! -z "$gpg_key" ]; then
-    packages=( "*.tar.zst" )
-    for name in $packages
-    do
-        gpg --detach-sig --yes $name
-    done
-    repo-add --verify --sign "./${repo_name:?}.db.tar.gz" ./*.tar.zst
-fi
 rclone copy ./ "onedrive:${dest_path:?}" --copy-links
