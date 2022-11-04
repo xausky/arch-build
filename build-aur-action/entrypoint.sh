f530ca7b62d7b1e92a06f4e441ac82d409cbdd27
xausky <xausky@gmail.com>
2022-11-04 11:04:37 +0800

    update

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -2,7 +2,8 @@
 
 pkgname=$1
 
-pacman -Sy --noconfirm
+pacman -Suy --noconfirm
+pacman -S base-devel git --noconfirm
 pacman -U --noconfirm /yay.pkg.tar.zst
 mkdir ./build
 yay -S --noconfirm --builddir=./build $pkgname
