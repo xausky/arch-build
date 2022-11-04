435abf3208d683a3cc281f6f652777d06e81bb7c
xausky <xausky@gmail.com>
2022-11-04 11:31:19 +0800

    update

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -13,3 +13,6 @@ pacman -U --noconfirm /yay.pkg.tar.zst
 mkdir ./build
 chown builder:builder -R ./build
 sudo --set-home -u builder yay -S --noconfirm --builddir=./build $pkgname
+mkdir ./build/dist
+cp ./build/*/*.tar.zst ./build/dist
+repo-add "./build/dist/xausky.db.tar.gz" ./build/dist/*.tar.zst
