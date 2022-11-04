d87cf2d575139a08c325538acb4de6ba1c892417
xausky <xausky@gmail.com>
2022-11-04 11:21:08 +0800

    update

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -3,6 +3,7 @@
 pkgname=$1
 
 useradd builder -m
+echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
@@ -10,5 +11,5 @@ pacman -Suy --noconfirm
 pacman -S base-devel git --noconfirm
 pacman -U --noconfirm /yay.pkg.tar.zst
 mkdir ./build
-chown builder:builder -R .
+chown builder:builder -R ./build
 sudo --set-home -u builder yay -S --noconfirm --builddir=./build $pkgname
