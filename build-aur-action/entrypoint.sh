f5801a5a47406d5b2a51f019b80a34a427ef17e4
xausky <xausky@gmail.com>
2022-11-04 18:07:56 +0800

    update

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -11,12 +11,9 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
 pacman -Suy --noconfirm
-pacman -S base-devel git --noconfirm
-pacman -U --noconfirm /yay.pkg.tar.zst
-mkdir ./build
-chown builder:builder -R ./build
-sudo --set-home -u builder yay -S --noconfirm --builddir=./build $pkgname
-rm -r ./build/dist
-mkdir ./build/dist
-cp ./build/*/*.tar.zst ./build/dist
-repo-add "./build/dist/xausky.db.tar.gz" ./build/dist/*.tar.zst
+pacman -S base-devel git python-pip pyalpm --noconfirm
+pip install pikaur
+sudo --set-home -u builder python -m pikaur -Sw --noconfirm $pkgname
+mkdir ./dist
+cp ~/.cache/pikaur/pkg/*.tar.zst ./dist
+repo-add "./dist/xausky.db.tar.gz" ./dist/*.tar.zst
